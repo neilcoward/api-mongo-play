e8cf3d52f3855543614a67fe7ab9e3ec0f5da561
Neil Coward <neilcoward@hotmail.co.uk>
2015-09-10 20:33:35 +0100

    vscode update and new route added

--- src/server/routes/bookRoutes.js
+++ src/server/routes/bookRoutes.js
@@ -1,15 +1,18 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var Grid = require('gridfs-stream');
+var fs = require('fs');
 
-var routes = function(Book) {
+var routes = function (Book) {
 	var bookRouter = express.Router();
 	var bookController = require('../controllers/bookController')(Book);
 
 	bookRouter.route('/Books')
-	.post(bookController.post)
-	.get(bookController.get);
+		.post(bookController.post)
+		.get(bookController.get);
 
-	bookRouter.use('/Books/:bookId', function(req, res, next) {
-		Book.findById(req.params.bookId, function(err, book) {
+	bookRouter.use('/Books/:bookId', function (req, res, next) {
+		Book.findById(req.params.bookId, function (err, book) {
 			if (err) {
 				res.status(500).send(err);
 			} else if (book) {
@@ -19,6 +22,16 @@ var routes = function(Book) {
 			else {
 				res.status(404).send('no book found');
 			}
+		});
+	});
+
+	bookRouter.use('/Books/:bookId/Cover/:coverId', function (req, res, next) {
+		Grid.mongo = mongoose.mongo;
+		var conn = mongoose.connection;
+		/* jshint -W064 */
+		var gfs = Grid(conn.db);
+		gfs.findOne({ _id: req.params.coverId }, function (err, file) {
+			console.log(file);
 		});
 	});
 
